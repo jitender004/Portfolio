4192d18b22a5d9cc25995ad5574720a7f20c5f99
JITENDER <55375274+jitender004@users.noreply.github.com>
2022-12-06 08:52:03 +0530

    init

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ export const headerData = {
   // title: "Full Stack Web Developer",
   desciption:
     "An aspiring full-stack web developer specialized in Mern stack, who focuses on writing clean, elegant, and efficient code. Looking forward to work in a challenging and progressive work environment.",
-  image: "https://www.linkpicture.com/q/1_2802.jpg",
+  image: "https://ibb.co/P9H3xqt",
   resumePdf:
     "https://drive.google.com/file/d/17Y85YR419eJcUx15oGkJ663iq8SuWZTR/view?usp=sharing",
 };
